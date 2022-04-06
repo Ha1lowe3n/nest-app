d0b14f8fbda585a03e133639431aa6a780933cd9
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-04-06 09:54:00 +0800

    connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { getMongoConfig } from './configs/mongo.config';
 import { PageModule } from './page/page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 
 @Module({
+	imports: [
+		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
+		AuthModule,
+		PageModule,
+		ProductModule,
+		ReviewModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
-	imports: [AuthModule, PageModule, ProductModule, ReviewModule],
 })
 export class AppModule {}
